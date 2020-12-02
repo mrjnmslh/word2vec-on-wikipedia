d6fffede1065c2f455e82264f04359aed4577ddd
mrjnmslh <44518604+mrjnmslh@users.noreply.github.com>
2020-12-02 11:52:30 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-# download the latest English wikipedia dump
-curl -L -O "https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2"
+# download the latest fa wikipedia dump
+curl -L -O "https://dumps.wikimedia.org/fawiki/latest/fawiki-latest-pages-articles.xml.bz2"
 
 # extract and clean texts from downloaded wikipedia dump
 mkdir -p extracted
@@ -20,7 +20,7 @@ cat processed/wiki_* > processed/wiki
 # train the word embeddings using skipgram model
 mkdir -p results
 cd word2vec
-./word2vec -train ../processed/wiki -output ../results/enwiki.skip.size300.win10.neg15.sample1e-5.min15.bin -cbow 0 -size 300 -window 10 -negative 15 -hs 0 -sample 1e-5 -threads 24 -binary 1 -min-count 15
+./word2vec -train ../processed/wiki -output ../results/fawiki.skip.size300.win10.neg15.sample1e-5.min15.bin -cbow 0 -size 300 -window 10 -negative 15 -hs 0 -sample 1e-5 -threads 24 -binary 1 -min-count 15
 
 # test the performace of word embeddings on word relation test set
-./compute-accuracy ../results/enwiki.skip.size300.win10.neg15.sample1e-5.min15.bin < questions-words.txt
+./compute-accuracy ../results/fawiki.skip.size300.win10.neg15.sample1e-5.min15.bin < questions-words.txt
